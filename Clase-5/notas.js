03c595821fe75b31119b2995191dd6b1a5ea5c55
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2023-10-18 12:10:34 -0300

    clase 5

---
+++ Clase-5/notas.js
@@ -0,0 +1,62 @@
+const producto={
+    nombre:"remera",
+    precio:20000
+}
+
+
+
+//acciones 
+
+//DB -> crear, manipular (modificar o agregar),obtener y eliminar info
+
+/* DB no relacionales */
+//usan colecciones -> JSON listas de objetos 
+//MongoDB -> sistema de gestion de DB no relacional
+
+
+//firebase -> usa esta idea de db no relacional y agrega mas cositas...
+
+/* DB relacionales */
+
+//utiliza tablas para guardar la informacion
+//filas -> registros
+//columnas -> campos 
+//MySQL - MariaDB-> sistema de gestion de DB relacional
+
+//XAMPP -> programa 
+//Workbench
+
+//SQL -> lenguaje
+
+
+//ejemplo
+//campos: nombre - precio descuento
+// registros : remera - 20000 - 1000
+
+
+//JS
+//strings - numbers - boolean
+
+//SQL
+//VARCHAR -> caracteres (seria como string pero hasta 265 caracteres)
+
+//INTEGER ->numeros enteros
+
+//DATE ->  fecha
+
+
+
+// protocolo HTTP
+
+///reglas ->metodos : GET->pedimos informacion ; POST-> mandando informacion
+
+
+//Cronograma semana que viene
+
+//proxima clase 23/10 -> lenguaje SQL (comandos y como hacer la db , tablas, registros y modificaicones sin usar la botonera del panel).+ practica(Lunes)
+
+//clase 25/10 -> Ejercitacion , tp en equipo SQL(Miercoles)
+
+//clase 27/10 -> Intro a DB no relacional (Viernes)
+
+//taller de TypeScript ->26/10 19hs (Jueves) 
